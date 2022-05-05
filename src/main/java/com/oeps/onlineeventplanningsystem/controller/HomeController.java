d73a0760c8c87850b68b595289b8fa39214fda60
Fazid Samoon <tuan.fazid@outlook.com>
2022-05-05 14:59:26 +0530

    Update HomeController.java

--- src/main/java/com/oeps/onlineeventplanningsystem/controller/HomeController.java
+++ src/main/java/com/oeps/onlineeventplanningsystem/controller/HomeController.java
@@ -3,9 +3,6 @@ package com.oeps.onlineeventplanningsystem.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 
 @Controller
 public class HomeController {
